7a6d8deb74f7fbc526a75de211046eb6ee258f7a
ilonaurlik <37349626+ilonaurlik@users.noreply.github.com>
2018-08-23 01:49:59 +0200

    Javadoc

--- SplitExpenses/src/com/splitexpenses/main/Main.java
+++ SplitExpenses/src/com/splitexpenses/main/Main.java
@@ -9,6 +9,12 @@ import com.splitexpenses.domain.Payment;
 import com.splitexpenses.domain.Person;
 import com.splitexpenses.service.PersonService;
 
+/**
+ * The program divides the expenses between the group of people. 
+ * You can add all expenses which should be shared among the group of people and the program will calculate 
+ * each person's current balance, as well as who owes what (and to whom).
+ */
+
 public class Main {
 	
 	private static final PersonService personService = new PersonService();
